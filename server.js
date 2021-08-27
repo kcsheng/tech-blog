215d1438958032a696bfb7bb84f341c97ba56d7a
kcsheng <kacysheng@mail.com>
2021-08-27 16:31:17 +1000

    Connect blog_db to the server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require(express);
+const express = require("express");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,8 @@ app.get("/", (req, res) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-  console.log(`App listening on PORT ${PORT}`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+  });
 });
