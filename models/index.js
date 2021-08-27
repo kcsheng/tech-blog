9615061f13ecec5f4457a0cd25b871261cba88e5
kcsheng <kacysheng@mail.com>
2021-08-28 08:11:06 +1000

    Add relationships between models

---
+++ models/index.js
@@ -0,0 +1,29 @@
+const User = require("./User");
+const Article = require("./Article");
+const Comment = require("./Comment");
+
+User.hasMany(Article, {
+  foreignKey: "creator_id",
+});
+
+Article.belongsTo(User, {
+  foreignKey: "creator_id",
+});
+
+Article.hasMany(Comment, {
+  foreignKey: "article_id",
+});
+
+Comment.belongsTo(Article, {
+  foreignKey: "article_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "commentor_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Article, Comment };
