30598bb6b9decaf31836a9fd951eedfc6bf68652
kcsheng <kacysheng@mail.com>
2021-08-28 11:09:23 +1000

    Test home routes functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,14 @@ const router = require("express").Router();
 const { User, Article, Comment } = require("../models");
 
 router.get("/", async (req, res) => {
-  const allUsers = await User.findAll();
+  const allUsers = await User.findAll({
+    include: [
+      {
+        model: Article,
+        attributes: ["title", "content", "createdAt"],
+      },
+    ],
+  });
   res.json(allUsers);
 });
 
