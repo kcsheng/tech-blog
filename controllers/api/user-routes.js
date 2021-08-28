8229072921df177ce0f7189474fa92aac61ae6f7
kcsheng <kacysheng@mail.com>
2021-08-28 19:18:53 +1000

    Implement post request to create a new user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,28 @@
+const { User } = require("../../models");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("This is from api users route");
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    console.err(err);
+  }
 });
 
+// Create a new user
+router.post("/", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const userPostedData = await User.create({
+      username,
+      password,
+    });
+    res.status(200).json(userPostedData);
+    res.redirect("/dashboard");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
