4129ea7e0c2f093329de727989c3f80b1e679312
kcsheng <kacysheng@mail.com>
2021-08-28 11:53:38 +1000

    Set up basic view structure

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const { User, Article, Comment } = require("../models");
 
 router.get("/", async (req, res) => {
-  const allUsers = await User.findAll({
-    include: [
-      {
-        model: Article,
-        attributes: ["title", "content", "createdAt"],
-      },
-    ],
-  });
-  res.json(allUsers);
+  // const allUsers = await User.findAll({
+  //   include: [
+  //     {
+  //       model: Article,
+  //       attributes: ["title", "content", "createdAt"],
+  //     },
+  //   ],
+  // });
+  res.render("homepage");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+const path = require("path");
 const express = require("express");
 const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+
 const routes = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const hbs = exphbs.create({});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 sequelize.sync({ force: false }).then(() => {
