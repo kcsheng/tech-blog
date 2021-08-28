00cb2ee6db970f9a0ca27ce82ad64d5a38ba9229
kcsheng <kacysheng@mail.com>
2021-08-28 13:02:45 +1000

    Add bootstrap and js

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const hbs = exphbs.create({});
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
+app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
