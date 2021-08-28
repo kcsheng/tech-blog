dc5930a8ff83b1e273b534b46d5e3ea98f9f0e91
kcsheng <kacysheng@mail.com>
2021-08-28 23:25:52 +1000

    Set up session in the server

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const path = require("path");
 const express = require("express");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const sessionConfig = {
+  secret: "xcjsvzo934dklfg34=5-a-secret-key",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sessionConfig));
 
 const hbs = exphbs.create({ helpers });
 app.engine("handlebars", hbs.engine);
